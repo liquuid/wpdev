435be2b90d0f72d6a35e7cfe52edf649e9288932
Fernando Henrique <liquuid@gmail.com>
2017-11-29 12:31:29 -0200

    update to xenial

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,7 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = "ubuntu/trusty64"
+  config.vm.box = "ubuntu/xenial64"
   config.vm.network "forwarded_port", guest: 80, host: 8000
   config.vm.network :private_network, ip: "192.168.78.50"
 
@@ -14,7 +14,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # config.ssh.forward_agent = true
 
   config.vm.synced_folder "./", "/var/www/wordpress/", create: true,
-    owner: "vagrant",
+    owner: "ubuntu",
     group: "www-data",
     mount_options: ["dmode=775,fmode=664"]
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,30 +2,31 @@
 
 DB_NAME="wordpress"
 LOCAL_BIN="/usr/local/bin"
-VAGRANT_PATH="/var/www/wordpress"
+VAGRANT_PATH="/vagrant"
 
 echo "---> Installing apt packages"
-apt-get -y update && \
-apt-get -y upgrade && \
-DEBIAN_FRONTEND=noninteractive apt-get -y install unzip npm nodejs mysql-client mysql-server python-mysqldb php5 php5-cli php5-curl php5-gd php5-imagick php5-mysql php5-xmlrpc apache2 libapache2-mod-php5 && \
+apt -y update && \
+apt -y upgrade && \
+DEBIAN_FRONTEND=noninteractive apt -y install unzip npm nodejs mysql-client mysql-server python-mysqldb php7.0 php7.0-cli php7.0-curl php7.0-gd php-imagick php7.0-mysql php7.0-xmlrpc apache2 libapache2-mod-php7.0 && \
 
 echo "---> Enable mod rewrite"
 a2enmod rewrite && \
 
-echo "---> Adding vagrant user to admin groups"
-usermod -a -G www-data,adm,root vagrant && \
+echo "---> Adding ubuntu user to admin groups"
+usermod -a -G www-data,adm,root ubuntu && \
 
-echo "---> Adding vagrant new home user"
-usermod -d $VAGRANT_PATH vagrant && \
+echo "---> Adding ubuntu new home user"
+usermod -d $VAGRANT_PATH ubuntu && \
 
-echo "---> Adding vagrant user to admin groups"
+echo "---> Adding ubuntu user to admin groups"
 usermod -a -G adm,root www-data && \
 
 echo "---> Creating database"
 { mysqladmin -u root create $DB_NAME ; } || { echo "ok" ; } 
 
 echo "---> Populating database"
-mysql -u root $DB_NAME < /var/www/wordpress/db/base.sql && \
+mysql -u root $DB_NAME < /vagrant/db/base.sql && \
+echo "grant all privileges on wordpress.* to 'ubuntu'@'%' identified by 'mysql';" | mysql && \
 
 echo "---> Install wp-cli"
 wget -q -c https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O $LOCAL_BIN/wp && \
@@ -34,21 +35,17 @@ chmod -v 755 $LOCAL_BIN/wp && \
 echo "---> Configuring apache2 vhost"
 cp -v $VAGRANT_PATH/templates/vhost.conf /etc/apache2/sites-available/ && \
 
+echo "--> Installing wordpress"
+./get_wordpress.sh
+
 echo "---> Configuring wp-config"
-cp -v $VAGRANT_PATH/templates/wp-config-sample.php /var/www/wordpress/src/wp-config.php && \
+cp -v $VAGRANT_PATH/templates/wp-config-sample.php $VAGRANT_PATH/src/wp-config.php && \
 
 echo "---> cp htaccess template"
-cp -v /var/www/wordpress/src/_htaccess /var/www/wordpress/src/.htaccess
+cp -v $VAGRANT_PATH/src/_htaccess $VAGRANT_PATH/src/.htaccess
 
 echo "---> Create symbolic link to nodejs"
 ln -fvs /usr/bin/nodejs /usr/bin/node
-
-echo "---> Install node packages"
-su - vagrant -c 'cd /var/www/wordpress/src; /usr/bin/npm --silent install' && \
-/usr/bin/npm -q install -g grunt-cli && \
-/usr/bin/npm -q install -g gulp && \
-
-su - vagrant -c 'cd /var/www/wordpress/src; /usr/local/bin/grunt less' && \
 
 echo "---> Enable site vhost"
 a2ensite vhost && \
